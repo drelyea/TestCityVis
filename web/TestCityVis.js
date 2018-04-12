0f09b1cb2bef01e650e4559551d86c625f93c75f
kaylidoll <kkdoll4@yahoo.com>
2018-04-12 19:19:30 -0400

    when you click age or income (on or off), the circles slide

--- web/TestCityVis.js
+++ web/TestCityVis.js
@@ -1,86 +1,140 @@
-var width = 960; 
-var height = 600; 
 var svg = d3.select("svg");
-svg.attr("width", width).attr("height", height);
 
   var margin = {top: 20, right: 20, bottom: 20, left: 40},
       width = 960 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
-      svgWidth=950;
-      svgHeight=800;
-
-  // setup x 
-  var xScale = d3.scaleLinear().range([0, width]), 
-      xAxis = d3.axisBottom(xScale);
-
-  // setup y
-  var yScale = d3.scaleLinear().range([height, 0]), 
-      yAxis = d3.axisLeft(yScale);
+      height = 600 - margin.top - margin.bottom,
+      svgWidth=960,
+      svgHeight=600;
 
 
-    xScale.domain([0,100]);
-    yScale.domain([0, 100]);
+svg.attr("width", svgWidth).attr("height", svgHeight);
 
-  var group = svg.append('g');//.attr('transform', "translate(" + (width+margin.left+margin.right) + "," + 500 + ")");
+// setup x 
+var xScale = d3.scaleLinear().range([0, width]), 
+    xAxis = d3.axisBottom(xScale);
 
+// setup y
+var yScale = d3.scaleLinear().range([height, 0]), 
+    yAxis = d3.axisLeft(yScale);
 
-      // x-axis
-    group.append("g")
-        .attr("transform", "translate(0," + yScale(0) + ")")
-        .call(xAxis);
 
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-/*d3.json("top100.json", function(error, graph) {
-  if (error) throw error;
 
-  var lines = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-    .attr("stroke-width", function(d) { 
-      return Math.sqrt(d.value); 
+xScale.domain([28,44]);
+yScale.domain([0, 100]);
+
+var group = svg.append('g');//.attr('transform', "translate(" + (width+margin.left+margin.right) + "," + 500 + ")");
+
+
+  // x-axis
+var axis=group.append("g")
+  .attr("transform", "translate(0," + yScale(0) + ")")
+  .call(xAxis);
+
+var rollover=group.append('text').style("text-anchor", "left").style("font-size", "25px").style("width",100).attr('transform', "translate(" + 50 + "," + 100 + ")");
+
+
+d3.json("../data/capital_data.json", drawPoints);
+function drawPoints(error, points){
+  var capitals = points["capitals"];
+
+
+  console.log(points);
+
+  svg.selectAll('circle')
+    .data(capitals).enter()
+    .append('circle')
+    .attr('cx', function(d,i) {
+      //console.log("age" + d.age);
+      //console.log("i" + i);
+     
+      return xScale(d.age);
+         
+    })
+    .attr('cy', function(d,i) {
+      return yScale(Math.random() * 100);
+    })
+    .attr('r', function(d,i) {
+      return Math.log(d.pop/1000);//Math.log(d.pop);
+    })
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'black')
+    .on('mouseover', function(d) {      
+      rollover.text("Rolled Over: " + d.name);
+      //group.style("fill", color[d.label]);
+
+      //fillRectangle(d.id);  
+    })
+    .on('mouseleave', function(d) {      
+      rollover.text("");
+      //group.style("fill", "white");
+      //group.style("background-color", "blue");
+
+      //fillWhiteRectangle();
     });
+}
 
-  var circles = svg.append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.nodes)
-    .enter().append("circle")
-    .attr("r", 5)
-    .attr("fill", function(d) { 
-      return color(d.group); 
-    });
 
-  // When hovering a node, show its id 
-  circles.append("title")
-      .text(function(d) { return d.id; });
+function readJson(category,max,min){
+  xScale.domain([min,max]);
+  yScale.domain([0, 100]);
 
-  var linkForce = d3.forceLink()
-    .links(graph.links)
-    .id(function(d) { 
-      return d.id; 
-    });
 
-  var simulation = d3.forceSimulation(graph.nodes)
-      .force("link", linkForce)
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .on("tick", ticked);
-      
-  function ticked() {
-    circles
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    // x-axis
+  axis.call(xAxis);
 
-    lines
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+
+
+  d3.json("../data/capital_data.json", drawPoints);
+  function drawPoints(error, points){
+    var capitals = points["capitals"];
+
+
+    console.log(points);
+
+    svg.selectAll('circle')
+      .transition()
+
+      .attr('cx', function(d,i) {
+        //console.log("age" + d.age);
+        //console.log("i" + i);
+
+
+        switch(category) {
+          case "Age":
+              console.log(category);
+              return xScale(d.age);
+              break;
+          case "Income":
+              console.log(category);
+              return xScale(d.income);
+              break;
+          default:
+              return xScale(d.age);
+        }
+      })
+      .attr('cy', function(d,i) {
+        return yScale(Math.random() * 100);
+      })
+      .attr('r', function(d,i) {
+        return Math.log(d.pop/1000);//Math.log(d.pop);
+      })
+      .attr('fill', 'steelblue')
+      .attr('stroke', 'black')
+      /*.on('mouseover', function(d) {      
+        rollover.text("Rolled Over: " + d.name);
+        //group.style("fill", color[d.label]);
+
+        //fillRectangle(d.id);  
+      })
+      .on('mouseleave', function(d) {      
+        rollover.text("");
+        //group.style("fill", "white");
+        //group.style("background-color", "blue");
+
+        //fillWhiteRectangle();
+      })*/;
   }
-});
-
-*/
+}
